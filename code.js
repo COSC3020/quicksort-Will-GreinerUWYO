9f088eaae5759cd49261dbe3122540f62558bb94
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-04 22:41:12 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,49 +1,47 @@
 function quicksort(array) {
-    if (array.length <= 1) {
-        return array;
-    }
-    let top = 0;
-    let lo = 0;
-    let hi = array.length - 1;
-    let stack = [];
+    let endpoint = array.length - 1;
     
-    stack[top] = lo;
-    stack[++top] = hi;
+    let stack = [];
+    stack.push(0);
+    stack.push(endpoint);
+    
+    while (stack.length > 0) {
+        let end = stack.pop();
+        let start = stack.pop();
 
-    while (top >= 0) {
-        hi = stack[top--];
-        lo = stack[top--];
+        let pivot = partition(array,start,end);
 
-        let pivot = partition(array, lo, hi);
-        if (pivot - 1 > lo) {
-            stack[++top] = lo;
-            stack[++top] = pivot - 1;
+        if (pivot - 1 > start) {
+            stack.push(start);
+            stack.push(pivot - 1);
         }
-        if (pivot + 1 < hi) {
-            stack[++top] = pivot + 1;
-            stack[++top] = hi;
+
+        if (pivot + 1 < end) {
+            stack.push(pivot + 1);
+            stack.push(end);
         }
+
     }
+
     return array;
 }
 
-//swap function
-function swap(array, a, b) {
-    let temp = array[a];
-    array[a] = array[b];
-    array[b] = temp;
-    return array; 
-}
 
-//partition from class slides
-
-function partition(array, lo, hi) {
-    let pivot = lo;
-    for (let i = lo + 1; i <= hi; i++) {
-        if (array[i] < array[lo]) {
-            swap(array, ++pivot, i);
+function partition(array,start,end) {
+    let pivot = array[end];
+    let i = start - 1;
+    let temp;
+    for (let j = start; j < end; j++) {
+        if (array[j] < pivot) {
+            i++;
+            temp = array[i];
+            array[i] = array [j];
+            array[j] = temp;
         }
     }
-    swap(array, lo, pivot);
-    return pivot;
+    i++;
+    temp = array[i];
+    array [i] = array[end];
+    array[end] = temp;
+    return i;
 }
